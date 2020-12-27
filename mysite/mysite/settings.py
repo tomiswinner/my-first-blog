70eae766ed80475f36692f7f5186c085149cf506
tomiswinner <mouseofsack911@gmail.com>
2020-12-27 10:45:48 +0900

    4th commit coz ALLOWED_HOST was hollowed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7ewr$r5=6x985sm*u^)c79$)(lf*$7-7a)fiq=sy(fnag%11jv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
